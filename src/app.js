2f0bf84f390625d5283ffe7990a7e3eaf6ace00d
Danilo Augusto <danilloagt@gmail.com>
2024-02-10 19:53:21 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
 const routerAPI1 = require("./routes/routerApiV1");
 const END_POINT_V1 = process.env.END_POINT_V1;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(END_POINT_V1, routerAPI1);
